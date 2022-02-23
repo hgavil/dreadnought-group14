350d3149fa62a7290398d7d3bdf99bb65ff5992b
Hannah Gavilan <hgavil2@uic.edu>
2022-02-23 14:05:42 -0600

    - skeleton layout for main - game class created

---
+++ src/main/java/Game.java
@@ -0,0 +1,27 @@
+// main game logic
+
+import tools.Pair;
+
+public class Game {
+
+    private Pair<String, Float> scoreboard;
+    private int gameMode;
+
+    Game() {
+
+    }
+
+
+    public void matchMaking(User user) {
+        throw new Error("Not yet implemented");
+    }
+
+    public void setupGame() {
+        // player 1 picks (temp) 3 ships that they want to use, and where they will be placed on the map
+        // 1. pick ship
+        // 2. choose coordinates
+
+        // now player 2 does it
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,42 @@
 public class Main {
 
     public static void main(String[] args) {
-        // do something here...
+        /* initialization phase */
+
+        // create game instance for the game to begin
+
+        // create player instances
+
+        // create map
+
+
+
+        /* setup phase */
+        /* all console prints done in main, the rest is done in the game class */
+
+        // run setup method
+
+        // console prints introductory message
+
+        // console prints directions for the player
+        // player 1 picks (temp) 3 ships that they want to use, and where they will be placed on the map
+        // 1. pick ship
+        // 2. choose coordinates
+
+        // now player 2 does it
+
+
+
+        /* game phase */
+
+        // console prints game start messages and turn swap messages
+
+
+
+        /* game end phase */
+
+        // console prints an ending message and exits
+
+
     }
 }

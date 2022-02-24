272e6e4a173f28f6155fa3f9352f09d1196ef61b
Joseph Lenaghan <77464979+JLen28@users.noreply.github.com>
2022-02-23 21:55:24 -0600

    Update Corvette.java

--- src/main/java/Ships/Corvette.java
+++ src/main/java/Ships/Corvette.java
@@ -5,11 +5,19 @@ public class Corvette extends Spaceship {
     int numberOfShots = 1;
     int health = 2;
 
-    public int getSpecialAttack() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
+	//Corvette activates slip space thrusters for a one-time free 3 units of movement...
+	public int getSpecialAttack() {
+		System.out.println("Corvette thrusters are primed!\n");
+		Square[][][] curPos = new Square[getXPos()][getYPos()][0];
+		int val = getYPos();
+		val += 3;
+		if(val > 10) {
+			System.out.print("boost cannot be completed!\n");
+			return 0; // unsuccesful
+		}
+		setYPos(val);
+		return 1; //successful
+	}
     public String getName() {
         return name;
     }

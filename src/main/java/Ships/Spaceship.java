e8184937125133266e07a2997e37f0dfc8f24692
Hannah Gavilan <hgavil2@uic.edu>
2022-02-24 15:07:03 -0600

    - missed a bracket

--- src/main/java/Ships/Spaceship.java
+++ src/main/java/Ships/Spaceship.java
@@ -9,42 +9,43 @@ public abstract class Spaceship {
 	private int y = 0; // ships current y position
 	private Square[][] position = new Square[x][y];
 	private int health = 0;
+
 	public abstract int getSpecialAttack();
-	
+
 	public void changeHealth(int modifier) {
 		health += modifier;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public int getNumShots() {
 		return numberOfShots;
 	}
-	
+
 	public Square[][] getPosition() {
 		return position;
 	}
-	
-	
+
+
 	public void setXPos(int modifier) { // set the x position of the ship
 		position[x][y].changeOccupied(false); // old position no longer occupied
 		int x = modifier;
 		Square[][] nuPos = new Square[x][y];
-	    setPosition(nuPos);
-	    getPosition()[x][y].changeOccupied(true); // new position is occupied, update
+		setPosition(nuPos);
+		getPosition()[x][y].changeOccupied(true); // new position is occupied, update
 	}
-	
+
 	public void setYPos(int modifier) { // set the y position of the ship
 		position[x][y].changeOccupied(false); // old position no longer occupied
 		int y = modifier;
 		Square[][] nuPos = new Square[x][y];
-	    setPosition(nuPos);
-	    getPosition()[x][y].changeOccupied(true); // new position is occupied, update
+		setPosition(nuPos);
+		getPosition()[x][y].changeOccupied(true); // new position is occupied, update
 	}
-	
-	public void setXYPos(int modX,int modY) {
+
+	public void setXYPos(int modX, int modY) {
 		getPosition()[x][y].changeOccupied(false);
 		x = modX;
 		y = modY;
@@ -52,24 +53,26 @@ public abstract class Spaceship {
 		setPosition(nuPos);
 		nuPos[x][y].changeOccupied(true);
 	}
-	
+
 	public int getXPos() {
 		return x;
 	}
-	
+
 	public int getYPos() {
 		return y;
 	}
+
 	public void setPosition(Square[][] modifier) {
 		position = modifier;
 	}
-	
+
 	public void setName(String modifier) {
 		name = modifier;
 	}
-	
+
 	public void setNumberofShots(int modifier) {
 		numberOfShots = modifier;
-	
 
+
+	}
 }

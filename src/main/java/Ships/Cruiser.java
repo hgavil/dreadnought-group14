079e59b95946be57fb8ffc97569d57dd71f16ff5
Joseph Lenaghan <77464979+JLen28@users.noreply.github.com>
2022-02-23 21:56:41 -0600

    Update Cruiser.java

--- src/main/java/Ships/Cruiser.java
+++ src/main/java/Ships/Cruiser.java
@@ -5,10 +5,19 @@ public class Cruiser extends Spaceship {
     int numberOfShots = 3;
     int health = 3;
 
-    public int getSpecialAttack() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
+	public int getSpecialAttack() {
+		System.out.println("Cluster bombs away!\n");
+		Square[][][] attack = getPosition();
+		for(int i = 0; i < 3; i++) { // checking x positions...
+			for(int j = 0; i < 3; j++) { // checking y positions...
+				if(attack[getXPos() + i][getYPos() + j][0].hit == false) { // checking for a hit
+					attack[getXPos() + i][getYPos()+ j][0].changeHit();
+					return 1; 
+				}
+			}
+		}
+		return 1;
+	}
 
     public String getName() {
         return name;

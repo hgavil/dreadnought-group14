5fe840ec9f77251ea79e7d67b112328b4dc8a4cb
Joseph Lenaghan <77464979+JLen28@users.noreply.github.com>
2022-02-23 21:54:45 -0600

    Update Carrier.java

--- src/main/java/Ships/Carrier.java
+++ src/main/java/Ships/Carrier.java
@@ -5,11 +5,19 @@ public class Carrier extends Spaceship {
     int numberOfShots = 3;
     int health = 4;
 
-    public int getSpecialAttack() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
+	public int getSpecialAttack() {
 
+		System.out.println("Ion beam is fully charged!\n");
+		Square[][][] attack = getPosition();
+		for(int i = 1; i <= 5; i++) { // beam targets the next 5 squares in the direction that the ships x position was facing
+			if(attack[getXPos()+i][getYPos()][0].hit == false) { // checking for a hit
+				attack[getXPos()+i][getYPos()][0].changeHit(); // hit 
+				return 1; // successful attack
+			}
+		}		
+		return 0; // unsucessful attack
+	}
+    
     public String getName() {
         return name;
     }

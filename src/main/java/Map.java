b49d300daa510247f48e838d5449bdd76a966956
Jose Perez <jpere31@uic.edu>
2022-02-24 13:46:14 -0600

    initialized and made map 2D

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -4,28 +4,71 @@ import java.util.concurrent.*;
 public class Map {
     int SIZE = 10; // size of map 10x10x10
 
-    Square[][][] space;
+    // Square[][][] space;
+    Square[][] space;
 
+    /*
     public Map(){
-        space = new Square[SIZE][SIZE][SIZE];
-        // generate 100 random sprites
-        int rNum1, rNum2, rNum3;
-        int i = 0;
-        while (i < 100){
-            // nextInt is normally exclusive of the top value,
-            // so add 1 to make it inclusive
-            // ThreadLocalRandom.current().nextInt(min, max + 1)
-            rNum1 = ThreadLocalRandom.current().nextInt(0, 11);
-            rNum2 = ThreadLocalRandom.current().nextInt(0, 11);
-            rNum3 = ThreadLocalRandom.current().nextInt(0, 11);
+      space = new Square[SIZE][SIZE][SIZE];
 
-            if (space[rNum1][rNum2][rNum3].Occupied() == false){
-                space[rNum1][rNum2][rNum3].changeOccupied(true);
-                space[rNum1][rNum2][rNum3].changeItem(-1);
-                i = i+1;
-            }
-            // else repeat to fill up 100 squares
+      for(i=0; i<10; i++){
+        for(int j=0; j<10; j++){
+          for(int z=0; z<10; z++){
+            space[i][j][z] = new Square();
+          }
         }
+      }
+
+      // generate 100 random sprites
+      int rNum1, rNum2, rNum3;
+      i = 0;
+      while (i < 100){
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        // ThreadLocalRandom.current().nextInt(min, max + 1)
+        rNum1 = ThreadLocalRandom.current().nextInt(0, 10);
+        rNum2 = ThreadLocalRandom.current().nextInt(0, 10);
+        rNum3 = ThreadLocalRandom.current().nextInt(0, 10);
+
+        if (space[rNum1][rNum2][rNum3].Occupied() == false){
+          space[rNum1][rNum2][rNum3].changeOccupied(true);
+          space[rNum1][rNum2][rNum3].changeItem(-1);
+          i = i+1;
+        }
+        // else repeat to fill up 100 squares
+      }
     }
+    */
+
+  public Map(){
+    space = new Square[SIZE][SIZE];
+    int i;
+    
+    for(i=0; i<10; i++){
+      for(int j=0; j<10; j++){
+        space[i][j] = new Square();
+      }
+    }
+
+    // generate 100 random sprites
+    int rNum1, rNum2;
+    i = 0;
+    while (i < 100){
+      // nextInt is normally exclusive of the top value,
+      // so add 1 to make it inclusive
+      // ThreadLocalRandom.current().nextInt(min, max + 1)
+      rNum1 = ThreadLocalRandom.current().nextInt(0, 10);
+      rNum2 = ThreadLocalRandom.current().nextInt(0, 10);
+
+      if (space[rNum1][rNum2].Occupied() == false){
+        space[rNum1][rNum2].changeOccupied(true);
+        space[rNum1][rNum2].changeItem(-1);
+        i = i+1;
+      }
+      // else repeat to fill up 10 squares
+    }
+  }
+
+
 }
 

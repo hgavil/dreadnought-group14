1219d9be513dcdefd56131475f9668be8fe70d45
Jose Perez <jpere31@uic.edu>
2022-02-24 18:21:25 -0600

    round done but no return

--- src/main/java/Round.java
+++ src/main/java/Round.java
@@ -1,10 +1,12 @@
 import tools.Pair;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Vector;
 
 import Map.Terrain;
+import Ships.Spaceship;
 
 public class Round {
 private List<Player> players;
@@ -42,6 +44,10 @@ private int turn;
 
   public Round(Player p1, Player p2, Terrain t, Scanner in){
     turn = 0;
+    int hit = 0;    // to store who we hit: player, nothing, or sprite
+    int attack = 0; // which type of attack
+    int x = -1;        // x and y of where we attacked
+    int y = -1;
     //player 1/ turn 0 starts 
 
     while (p1.Ships().size() != 0 && p2.Ships().size() != 0){
@@ -49,18 +55,64 @@ private int turn;
       // player chooses to attack or do a special move
       // player 1
       if (turn == 0){
-        chooseAttack(p1, t, in);
+        attack = chooseAttack(p1, t, in);
       }
       else { // player 2
-        ;
+        attack = chooseAttack(p2, t, in);
       }
 
-      // alternate and repeat
+      if(attack == 1){
+        x = chooseXY(0, in);
+        y = chooseXY(0, in);
+        while (t.getMap().getSpace()[x][y].Hit()){
+          System.out.println(x+", "+y+" has been hit, please choose another pair\n");
+          x = chooseXY(0, in);
+          y = chooseXY(0, in);
+          // store what we hit
+          hit = t.getMap().getSpace()[x][y].Item();
+        }
+      }
+      else{
+        System.out.println("Special move to be implemented\n");
+        hit = 0;
+      }
+
+      // if we hit a player, remove ship from list and update map that we hit the spot
+      if (hit == 1){ // player 1
+        for(int i=0; i<p1.Ships().size(); i++){
+          if (p1.Ships().get(i).getXPos() == x && p1.Ships().get(i).getYPos() == y){
+            p1.Ships().remove(i);
+            break;
+          }
+        }
+      }
+      else if (hit == 2){ // player 2
+        for(int i=0; i<p2.Ships().size(); i++){
+          if (p2.Ships().get(i).getXPos() == x && p2.Ships().get(i).getYPos() == y){
+            p2.Ships().remove(i);
+            break;
+          }
+        }
+      }
+      else{ // non player
+        ; // no need to remove ship
+      }
+      // update spot hit
+      t.getMap().getSpace()[x][y].changeHit();
+
+      // alternate player and repeat
       nextTurn();
+    }
+
+    if (p1.Ships().size() == 0){
+      // player 2 won
+    }
+    else{
+      // player 1 won
     }
   }
 
-  private Boolean chooseAttack(Player p, Terrain t, Scanner in){
+  private int chooseAttack(Player p, Terrain t, Scanner in){
     Boolean safeInput = false;
     int userInput = -1;
     int x, y;
@@ -74,24 +126,18 @@ private int turn;
         // if it's something other than 1 or 2, continue
         if (userInput != 1 && userInput != 2)
           continue;
+        // safe to continue
+        safeInput = true;
         
-        if(userInput == 1){
-          do{
-            x = chooseXY(0, in);
-            y = chooseXY(0, in);
-
-          } while (t.map.space[x][y].Occupied())
-        }
-        else{
-          System.out.println("Special move to be implemented\n");
-        }
+        
       }
       else{
         System.out.println("Please choose 1 or 2");
         in.nextLine();
       }
     } while(!safeInput);
-    return false;
+    // hit nothing
+    return 0;
   }
 
   // val holds 0 for x and 1 for y

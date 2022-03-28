a35e6eae064242b74c2044448fb4dd6af90f20a0
Joseph Lenaghan <77464979+JLen28@users.noreply.github.com>
2022-03-28 15:16:36 -0600

    added special attack prototype

--- src/main/java/Ships/Carrier.java
+++ src/main/java/Ships/Carrier.java
@@ -1,22 +1,21 @@
 package Ships;
 
 import Map.Square;
-
+import Map.Map;
 public class Carrier extends Spaceship {
     String name = "Ships.Carrier";
     int numberOfShots = 3;
     int health = 4;
 
-	public int getSpecialAttack() {
+	public int getSpecialAttack(Map m) {
 
-		//System.out.println("Ion beam is fully charged!\n");
-		//Square[][] attack = getPosition();
-		//for(int i = 1; i <= 5; i++) { // beam targets the next 5 squares in the direction that the ships x position was facing
-			//if(attack[getXPos()+i][getYPos()].Hit() == false) { // checking for a hit
-				//attack[getXPos()+i][getYPos()].changeHit(); // hit 
-				//return 1; // successful attack
-			//}
-		//}		
+		System.out.println("Ion beam is fully charged!\n");
+		for(int i = 1; i <= 5; i++) { // beam targets the next 5 squares in the direction that the ships x position was facing
+		if(m.getSpace()[getXPos()+i][getYPos()].Hit() == false) { // checking for a hit
+			m.getSpace()[getXPos()+i][getYPos()].changeHit(); // hit 
+			return 1; // successful attack
+		}
+	}	
 		return 0; // unsucessful attack
 	}
     

e9ff37679c11a6e23c7851ee367b81336075c64e
Joseph <Joseph@DESKTOP-MF4UKR8.local>
2022-03-31 21:45:36 -0600

    added a check for board space

--- src/main/java/Ships/Carrier.java
+++ src/main/java/Ships/Carrier.java
@@ -11,6 +11,10 @@ public class Carrier extends Spaceship {
 
       System.out.println("Ion beam is fully charged!\n");
         for(int i = 1; i <= 5; i++) { // beam targets the next 5 squares in the direction that the ships x position was facing
+          if(getXPos() + i >= 10) {
+        	  System.out.println("attack exceeds boardspace! cannot complete attack");
+        	  return 0;
+          }
           if(m.getSpace()[getXPos()+i][getYPos()].Hit() == false && m.getSpace()[getXPos()+i][getYPos()].Item() > 0) { // checking for a hit
             m.getSpace()[getXPos()+i][getYPos()].changeHit(); // hit 
             return 1; // successful attack
